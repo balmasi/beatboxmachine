8a05a12a586d8c568b9ecd77c7b8d7c9b8833be7
Borna Almasi <borna@borna-almasi.com>
2013-01-24 21:46:18 -0500

    Initial Import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beatboxmachine::Application.config.secret_token = '043f120ae69d87dc4c0d317a05b5f4d72308f2c2f9f85b98d7877a869681a449ca2acc8ebaaacd88e004a21ba9d2b917865d317ab78f3eb075c9e5ff6862ebf0'
